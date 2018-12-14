74a0a0d418501a50f8d4e0fc3e435c8fbe30e0db
Jonathan <jonathankenton@gmail.com>
2018-12-14 11:18:53 +0200

    add full details to reservation

--- src/components/reservation/index.js
+++ src/components/reservation/index.js
@@ -8,6 +8,8 @@ const Reservation = ({ ...props }) => {
       <h1>The Jungle Fast Rider Service</h1>
       <div>Instructions</div>
       <ul>
+        <li>{props.reservation.ride.name}</li>
+        <li>{props.reservation.ride.zone.name}</li>
         <li>{props.reservation.return_time}</li>
         <li>{props.reservation.access_code}</li>
       </ul>
